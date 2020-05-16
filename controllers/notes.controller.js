f89ad2b797aa57921bf7c45e276c7419ada8756d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-16 19:37:54 +0530

    setted controller with function name

---
+++ controllers/notes.controller.js
@@ -0,0 +1,26 @@
+const Note = require('../models/Schema');
+
+// Create and save a new Note
+module.exports.create = function(req, res){
+
+}
+
+// Retrieve and return all notes from the database.
+module.exports.findAll = function(req, res){
+
+}
+
+// Find a single note with a note id
+module.exports.findOne = function(req, res){
+
+}
+
+// Update a note identified by the noteid in the request
+module.exports.update = function(req, res){
+
+}
+
+// Delete a note with the specified noteid in the request
+module.exports.delete = function(req, res){
+    
+}
