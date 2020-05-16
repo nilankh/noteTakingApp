aae66c20a5e647e820147e999e163ec20cb7070a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-16 19:43:59 +0530

    created first controller with definition creating a new note

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -2,7 +2,28 @@ const Note = require('../models/Schema');
 
 // Create and save a new Note
 module.exports.create = function(req, res){
+    // validate request
+    if(!req.body.content){
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
 
+    // create a note
+    const note = new Note({
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    });
+
+    // Save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occured while creating the Note."
+        });
+    });
 }
 
 // Retrieve and return all notes from the database.
@@ -22,5 +43,5 @@ module.exports.update = function(req, res){
 
 // Delete a note with the specified noteid in the request
 module.exports.delete = function(req, res){
-    
+
 }
