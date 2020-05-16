2295cde37c78af63078e52f9bd9ed6953622a7c2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-16 20:02:45 +0530

    setted up deleing a note controller and controller completed

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -94,7 +94,25 @@ module.exports.update = function(req, res){
     // The {new: true} option in the findByIdAndUpdate() method is used to return the modified document to the then() function instead of the original.
 }
 
+// Deleteing a note
 // Delete a note with the specified noteid in the request
 module.exports.delete = function(req, res){
-
+    Note.findByIdAndRemove(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send({message: "Note deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete note with id " + req.params.noteId
+        });
+    });
 }
